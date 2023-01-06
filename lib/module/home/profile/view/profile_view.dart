24c92c73a75aaecda1047f9adda6316d93090fd6
Fauzan Abdillah <74108522+fauzan2720@users.noreply.github.com>
2023-01-06 18:13:05 +0700

    edit profile

--- lib/module/home/profile/view/profile_view.dart
+++ lib/module/home/profile/view/profile_view.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:warung_nikmat/core.dart';
